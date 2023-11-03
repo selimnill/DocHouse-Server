c59795a492844c007de7c4bf173d00ad6cb52558
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-11-03 12:14:54 +0600

    vercel

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { add } = require('date-fns');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 
@@ -16,8 +17,20 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Welcome");
+    res.send("Welcome To The Server");
 });
+
+
+app.use(function (request, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    //intercept the OPTIONS call so we don't double up on calls to the integration
+    if ('OPTIONS' === request.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+  });
 
 
 
@@ -271,11 +284,29 @@ async function run() {
             res.send(result);
         });
 
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
         app.post('/payments', async (req, res) =>{
             const payment = req.body;
+            console.log(payment);
             const result = await paymentsCollection.insertOne(payment);
             const id = payment.bookingId
-            const filter = {_id: ObjectId(id)}
+            const filter = {_id: new ObjectId(id)}
             const updatedDoc = {
                 $set: {
                     paid: true,
